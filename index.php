ce6d434b4915447fb08b3b2648e78d76fa30a08e
sukansh <psukansh@gmail.com>
2022-05-10 19:40:53 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>kdkinternationals</title>
+	<link rel="stylesheet" href="style.css">
+	<script src="https://kit.fontawesome.com/56c759bb6e.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<div class="container">
+		<div class="homepage" id="home">
+			<nav>
+				<div class="logo">
+					<div class="name">
+						<h3>kdkinternationaltrade <i class="fad fa-signal-4"></i> </h3>
+					</div>
+					<div class="logo-icon">
+						<i class="fas fa-dot-circle"></i>
+					</div>
+				</div>
+
+				<div class="menu_list">
+					<ul>
+						<li><a href="#home">HOME</a></li>
+						<li><a href="#about">ABOUT</a></li>
+						<li><a href="#what">WHAT</a></li>
+						<li><a href="#brochure">BR0CHURE</a></li>
+						<li><a href="#gallery">GALLERY</a></li>
+						<li><a href="#contact">CONTACT</a></li>
+					</ul>
+				</div>
+			</nav>
+
+			<div class="banner">
+				<h1>Lorem ipsum dolor sit amet.</h1>
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugit vel, voluptates accusantium ex
+					obcaecati dolores porro exercitationem molestias ducimus.</p>
+				<a id="brochure-button" href="./images/brochure2.svg" download>
+					download brochure
+				</a>
+
+				<div class="below-slide">
+					<div class="ellipse">
+					<div class="point"></div>
+					</div>
+				</div>
+
+				<div class="up-slider">
+					<a href="#home">
+						<i class="fas fa-arrow-up"></i>
+					</a>
+				</div>
+
+				<div class="whatsapp">
+				<a href="https://wa.me/919309791707" target="black"><i class="fab fa-whatsapp"></i></a>	
+				</div>
+			</div>
+		</div>
+
+		<div class="about" id="about">
+			<div class="heading">
+				<h3>ABOUT <span>US</span></h3>
+				<h1>Lorem ipsum dolor sit.</h1>
+			</div>
+
+			<div class="info">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quo dicta minus error sapiente, ex
+					doloremque optio tenetur maiores quaerat sequi reprehenderit! Maxime perferendis, perspiciatis quis
+					consequuntur recusandae quos possimus non suscipit sint nobis reprehenderit atque iusto fugit autem
+					aut culpa ratione incidunt, voluptatum eveniet ex, delectus animi inventore dolore.</p>
+
+				<div class="link">
+					<a href="">Read More <i class="fas fa-arrow-right"></i></a>
+				</div>
+			</div>
+		</div>
+
+		<div class="what" id="what">
+			<h1>What we do</h1>
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis odit repudiandae voluptas doloribus
+				nostrum laborum quisquam placeat error facilis necessitatibus!</p>
+
+			<div class="content">
+				<div class="box">
+					<i class="fas fa-cogs"></i>
+					<h3>Development</h3>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit cupiditate corporis aliquam esse?
+					</p>
+				</div>
+
+				<div class="box">
+					<i class="fas fa-cogs"></i>
+					<h3>Development</h3>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit cupiditate corporis aliquam esse?
+					</p>
+				</div>
+
+				<div class="box">
+					<i class="fas fa-cogs"></i>
+					<h3>Development</h3>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit cupiditate corporis aliquam esse?
+					</p>
+				</div>
+
+				<div class="box">
+					<i class="fas fa-cogs"></i>
+					<h3>Development</h3>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit cupiditate corporis aliquam esse?
+					</p>
+				</div>
+
+				<div class="box">
+					<i class="fas fa-cogs"></i>
+					<h3>Development</h3>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit cupiditate corporis aliquam esse?
+					</p>
+				</div>
+
+				<div class="box">
+					<i class="fas fa-cogs"></i>
+					<h3>Development</h3>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit cupiditate corporis aliquam esse?
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="brochure" id="brochure">
+			<h1>Our brochure</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore architecto adipisci assumenda mollitia
+				omnis quos ipsam ad, aliquam eius eveniet!
+			</p>
+			<img src="./images/brochure2.svg" alt="" srcset="">
+		</div>
+
+		<div class="gallery" id="gallery">
+			<!-- <h1>Our Gallery<span>.</span></h1> -->
+			<!-- <div class="content"> -->
+				<!-- <img src="./images/home1.jpg" alt="" srcset="" width="400px" height="300px"> -->
+				<!-- <img src="./images/home2.jpg" alt="" srcset="" width="400px" height="300px"> -->
+				<!-- <img src="./images/home3.jpg" alt="" srcset="" width="400px" height="300px"> -->
+				<!-- <img src="./images/home1.jpg" alt="" srcset="" width="400px" height="300px"> -->
+				<!-- <img src="./images/home2.jpg" alt="" srcset="" width="400px" height="300px"> -->
+				<!-- <img src="./images/home3.jpg" alt="" srcset="" width="400px" height="300px"> -->
+			<!-- </div> -->
+		</div>
+
+		<br>
+		<br>
+
+		<!-- Slideshow container -->
+		<div class="slideshow-container">
+
+			<!-- Full-width images with number and caption text -->
+			<div class="mySlides fade">
+				<div class="numbertext">1 / 3</div>
+				<img src="./images/home1.jpg" style="width:700px">
+
+			</div>
+
+			<div class="mySlides fade">
+				<div class="numbertext">2 / 3</div>
+				<img src="./images/home2.jpg" style="width:700px">
+
+			</div>
+
+			<div class="mySlides fade">
+				<div class="numbertext">3 / 3</div>
+				<img src="./images/home3.jpg" style="width:700px">
+
+			</div>
+
+			<!-- Next and previous buttons -->
+			<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+			<a class="next" onclick="plusSlides(1)">&#10095;</a>
+		</div>
+		<br>
+
+		<!-- The dots/circles -->
+		<div style="text-align:center">
+			<span class="dot" onclick="currentSlide(1)"></span>
+			<span class="dot" onclick="currentSlide(2)"></span>
+			<span class="dot" onclick="currentSlide(3)"></span>
+		</div>
+
+
+		<div class="contact" id="contact">
+			<h1>Contact Us</h1>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, quasi? Pariatur voluptatibus eos eveniet
+				exercitationem quaerat, facilis ea quos eligendi.</p>
+			<div class="content">
+				<form action="">
+					<h3>Drop a line for us</h3>
+
+					<input type="text" placeholder="Name*">
+
+
+					<input type="text" placeholder="Email*">
+
+
+					<input type="text" placeholder="Message*">
+
+					<button type="submit"> <i class="fas fa-paper-plane"></i> Send</button>
+				</form>
+
+				<div class="address">
+					<h3>Our address</h3>
+					<p> <i class="fas fa-map-marker-alt"></i> Latare lay out , pandharkawada</p>
+					<p><i class="fas fa-envelope"></i> psukansh@gmail.com </p>
+					<p><i class="fas fa-phone-alt"></i> 9309791707</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="location">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15079.485137381931!2d77.29797765!3d19.11330185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1652187012251!5m2!1sen!2sin" width="1500" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+		</div>
+		<div class="footer">
+
+			<div class="social-media">
+				<h2>Follow us on</h2>
+				<ul>
+					<li><a href=""><i class="fab fa-facebook-square"></i></a></li>
+					<li><a href=""><i class="fab fa-instagram"></i></a></li>
+					<li><a href=""><i class="fab fa-twitter"></i></a></li>
+				</ul>
+			</div>
+			<p>@2022 kdkinternationals , all rights reserved.</p>
+		</div>
+	</div>
+
+</body>
+
+<script src="./index.js"></script>
+
+</html>
